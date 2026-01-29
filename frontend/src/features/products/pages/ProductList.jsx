9ccb1eb05911ce2d9030d0851f5db41d111ee7e0
talhaaazhar <talhaazhar474@gmail.com>
2026-01-29 14:55:12 +0500

    added seach functionality

--- frontend/src/features/products/pages/ProductList.jsx
+++ frontend/src/features/products/pages/ProductList.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ProductGrid from "../components/ProductGrid";
 import { MOCK_PRODUCTS } from "../services/mockProducts.jsx";
+import{useLocation} from"react-router-dom";
 
 
 function ProductList() {
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const searchTerm = queryParams.get("search")?.toLowerCase() || "";
+
+  // Filter products based on search query
+  const filteredProducts = MOCK_PRODUCTS.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm) ||
+    product.description.toLowerCase().includes(searchTerm)
+  );
+
   return (
     <section className="space-y-6 dark:bg-gray-900 dark:text-gray-100 p-6">
       <div>
